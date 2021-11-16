244f543a90b7c129a161fb68a3969f8fe2a9dafb
carlaklein <klein_carla@outlook.com>
2021-11-16 19:48:55 +0200

    dev: login, signup, db connection

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://Carla:%40Noodle3571@cluster0.5rbkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+module.exports = {
+    DATABASECONNECTION
+}
