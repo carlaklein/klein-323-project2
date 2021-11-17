3abd51947284538467507a1babe6842d109209a0
carlaklein <klein_carla@outlook.com>
2021-11-17 19:26:02 +0200

    date fix

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ var postSchema = mongoose.Schema({
     photo_caption: {type: String, required:true},
     geolocation: {type:String, required: true},
     tags: {type:String, required: true},
-    captured_date: {type:Date, required: true},
+    captured_date: {type:Date, default:Date.now},
     captured_by: {type:String, required:true},
     userID: {type:mongoose.Schema.Types.ObjectId,  required:false, unique:false}
 });
